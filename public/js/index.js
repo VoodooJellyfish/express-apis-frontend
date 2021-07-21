3a6530df0bd059e340c03a17e0ce9a6a4b48b03d
tannerpedretti <tannerpedretti@gmail.com>
2021-07-21 14:57:48 -0600

    we

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,23 @@
-console.log("Hello from index.js!");
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const res = await fetch("http://localhost:8080/tweets");
+        // if (res.status = 401) {
+        //     return res.redirect('/log-in')
+        // } else {
+            const { tweets } = await res.json();
+            const tweetsContainer = document.querySelector("#tweets-container");
+            const tweetsHtml = tweets.map(
+                ({ message }) => `
+                    <div class="card">
+                    <div class="card-body">
+                    <p class="card-text">${message}</p>
+                    </div>
+                    </div>`
+            );
+            tweetsContainer.innerHTML = tweetsHtml.join("");
+            console.log(tweets);
+        // } 
+    } catch (e) {
+        console.error(e);
+  }
+});
